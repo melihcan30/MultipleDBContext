2ab8e54ca1d636709190ad202750303c34fb7dcf
Melihcan Serbest <canmelih.free3@gmail.com>
2023-06-24 18:24:51 +0300

    Relationships between Databases

--- MultipleDBContext/Models/User.cs
+++ MultipleDBContext/Models/User.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public List<Character> Characters { get; set; } = new List<Character>();
+        
     }
 }
